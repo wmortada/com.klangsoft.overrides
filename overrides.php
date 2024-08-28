b4b3ab22f0a1f7785adad6c8cd36cccfb777b82c
William Mortada <williammortada@thirdsectordesign.org>
2024-08-28 10:00:42 +0100

    Civix upgrade

--- overrides.php
+++ overrides.php
@@ -254,59 +254,12 @@ function overrides_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function overrides_civicrm_postInstall(): void {
-  _overrides_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function overrides_civicrm_uninstall(): void {
-  _overrides_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function overrides_civicrm_enable(): void {
   _overrides_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function overrides_civicrm_disable(): void {
-  _overrides_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function overrides_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _overrides_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function overrides_civicrm_entityTypes(&$entityTypes): void {
-  _overrides_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
